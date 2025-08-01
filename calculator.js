75c1d18584c24fde83e8acb83543748e6724f08b
Manasi200609 <jadhavmanasi70@gmail.com>
2025-08-02 00:03:47 +0530

    added another variable to subtraction

--- calculator.js
+++ calculator.js
@@ -1,8 +1,10 @@
  function addition(a,b){
+    console.log("the first value is="+a);
+    console.log("the second number is="+b);
     return a+b;
 }
- function subtraction(a,b){
-    return a-b;
+ function subtraction(a,b,c){
+    return a-b-c;
 }
 console.log(addition(3,4));
-console.log(subtraction(6,2));
+console.log(subtraction(6,2,2));
