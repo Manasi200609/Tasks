5f2c47bd65e89d07e7a39cdb7181abbeb250ac45
Tulsiandhare11 <andharetulsi@gmail.com>
2025-08-02 11:32:40 +0530

    Added the changes in Test Branch

--- calculator.js
+++ calculator.js
@@ -1,8 +1,13 @@
- function addition(a,b){
-    return a+b;
+ function addition(a,b,c){
+    return a+b+c;
 }
  function subtraction(a,b){
     return a-b;
 }
-console.log(addition(3,4));
+function multiply(a,b){
+    return a*b;
+}
+
+console.log(addition(3,4,6));
 console.log(subtraction(6,2));
+console.log(multiply(4,2));
